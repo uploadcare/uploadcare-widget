d0ae31a7b31eda258f24300f1a3d122f1c48e1f4
Vadim Rastyagaev <abc@oktoberliner.ru>
2012-09-10 20:17:54 +0400

    tests

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -46,7 +46,7 @@ Dummy::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( uploadcare/widget.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -61,4 +61,5 @@ Dummy::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.js_compressor = :yui
 end
