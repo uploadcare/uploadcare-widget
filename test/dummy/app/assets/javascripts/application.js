dd3b47d277bfaf08767643225c19a8bfde21ff98
Valentin Golev <me@va1en0k.net>
2012-12-05 04:13:03 +0400

    staging social base for debugging (should have I?)

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -3,7 +3,8 @@ UPLOADCARE_PUBLIC_KEY = 'demopublickey';
 UPLOADCARE_URL_BASE = 'https://upload.staging0.uploadcare.com';
 UPLOADCARE_PUSHER_KEY = 'a2dfe15c549a403f58ee';
 
-UPLOADCARE_SOCIAL_BASE = 'http://0.0.0.0:5000';
+// UPLOADCARE_SOCIAL_BASE = 'http://0.0.0.0:5000';
+UPLOADCARE_SOCIAL_BASE = 'http://social.staging0.uploadcare.com/'
 
 setTimeout(function() {
     uploadcare.jQuery(uploadcare).on('uploadcare.debug', function(e, args){
