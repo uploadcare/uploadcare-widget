83262426c7fdb3bde4d2ceadea32024d626e0d12
homm <homm86@gmail.com>
2015-07-22 15:04:01 +0300

    move JstProcessor code to widget from test application

--- lib/uploadcare-widget.rb
+++ lib/uploadcare-widget.rb
@@ -3,3 +3,17 @@ require "uploadcare-widget/engine"
 
 module UploadcareWidget
 end
+
+require "sprockets"
+module Sprockets
+
+  class MyJstProcessor < JstProcessor
+
+    def evaluate(scope, locals, &block)
+      name = scope.logical_path.split('/').slice(2..-1).join('/')
+      "uploadcare.templates.JST[#{name.inspect}] = #{indent(data)};"
+    end
+  end
+
+  Sprockets.register_engine '.jst', MyJstProcessor
+end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -66,18 +66,3 @@ module Dummy
     config.assets.version = '1.0'
   end
 end
-
-
-require "sprockets"
-module Sprockets
-
-  class MyJstProcessor < JstProcessor
-
-    def evaluate(scope, locals, &block)
-      name = scope.logical_path.split('/').slice(2..-1).join('/')
-      "uploadcare.templates.JST[#{name.inspect}] = #{indent(data)};"
-    end
-  end
-
-  Sprockets.register_engine '.jst', MyJstProcessor
-end
