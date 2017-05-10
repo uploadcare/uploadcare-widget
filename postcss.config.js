100566a2be4df67b627722f181b4014144dc7262
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-05-10 19:11:53 +0300

    Update postcss config for upgraded postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,49 +1,30 @@
 const path = require('path')
-const argv = require('yargs').argv
 const stylesheetsPath = path.join(__dirname, 'app', 'assets', 'stylesheets', 'uploadcare')
 
-const withMinification = (argv.min) || false
-
-let use = [
-  'postcss-import',
-  'postcss-custom-media',
-  'postcss-nested',
-  'postcss-css-variables',
-  'postcss-calc',
-  'postcss-color-function',
-  'postcss-flexbugs-fixes',
-  'autoprefixer',
-]
-let configUse = {
-  'postcss-import': {
-    path: stylesheetsPath,
-    plugins: [
-      require('stylelint'),
-      require('postcss-prefixer')('uploadcare--', {
-        ignore: [
-          /^\.uploadcare-/,
-          /^\.ord-/,
-          '.bottom',
-          '.right',
-        ],
-      }),
-    ],
+module.exports = (ctx) => ({
+  plugins: {
+    'postcss-import': {
+      path: stylesheetsPath,
+      plugins: [
+        require('stylelint'),
+        require('postcss-prefixer')('uploadcare--', {
+          ignore: [
+            /^\.uploadcare-/,
+            /^\.ord-/,
+            '.bottom',
+            '.right',
+          ],
+        }),
+      ],
+    },
+    'postcss-custom-media': {},
+    'postcss-nested': {},
+    'postcss-css-variables': {},
+    'postcss-calc': {},
+    'postcss-color-function': {},
+    'postcss-flexbugs-fixes': {},
+    'autoprefixer': {},
+    'cssnano': ctx.env === 'production' ? {zindex: false} : false,
+    'postcss-reporter': {},
   },
-}
-
-const minificationUse = ['cssnano']
-const configMinificationUse = {'cssnano': {zindex: false}}
-
-if (withMinification) {
-  use = [...use, ...minificationUse]
-  configUse = Object.assign({}, configUse, configMinificationUse)
-}
-
-const config = {
-  'input': path.join(stylesheetsPath, 'styles.pcss'),
-  'output': path.join(stylesheetsPath, 'styles.css'),
-  'local-plugins': true,
-  'use': [...use, 'postcss-reporter'],
-}
-
-module.exports = Object.assign({}, config, configUse)
+})
