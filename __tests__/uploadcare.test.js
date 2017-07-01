f897030bf28f9a28d21b7fb37fd2efae58c70f0a
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-07-01 03:43:16 +0300

    Update test

--- __tests__/uploadcare.test.js
+++ __tests__/uploadcare.test.js
@@ -1,18 +1,35 @@
+/* eslint max-nested-callbacks: ["error", 3] */
 const assert = require('assert')
 const uploadcare = require('../pkg/latest/uploadcare')
+
+window.UPLOADCARE_PUBLIC_KEY = 'demopublickey'
+
+const bodyHasElement = (selector) => {
+  const $el = document.body.querySelector(selector)
+
+  assert.equal($el !== null, true)
+}
 
 describe('uploadcare', function() {
   it('has version', () => {
     assert.equal(typeof uploadcare.version, 'string')
   })
 
-  it('create widget', () => {
-    document.body.innerHTML = '<input role="uploadcare-uploader" type="hidden">'
+  describe('widget', () => {
+    it('create widget', () => {
+      document.body.innerHTML = '<input role="uploadcare-uploader" type="hidden">'
 
-    uploadcare.initialize()
+      uploadcare.initialize()
 
-    const $widget = document.body.querySelector('.uploadcare--widget')
+      bodyHasElement('.uploadcare--widget')
+    })
 
-    assert.equal($widget !== null, true)
+    it('click on widget create dialog', () => {
+      const $widget = document.body.querySelector('.uploadcare--widget__button_type_open')
+
+      $widget.click()
+
+      bodyHasElement('.uploadcare--dialog')
+    })
   })
 })
