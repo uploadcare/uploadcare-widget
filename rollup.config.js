b92f4e2606eb322b008fc839b4e7847003930f19
Aleksandr Grenishin <nd0ut.me@gmail.com>
2018-06-15 15:36:12 +0300

    setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import license from 'rollup-plugin-license'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import postcss from 'rollup-plugin-postcss'
+
+const getPlugins = () =>
+  [
+    replace({'process.env.NODE_ENV': process.env.NODE_ENV}),
+    resolve({jsnext: true}),
+    commonjs({
+      include: 'node_modules/**',
+      sourceMap: false,
+    }),
+    postcss({
+      modules: true,
+      plugins: [],
+    }),
+    babel(),
+    license({
+      banner: `
+      <%= pkg.name %> <%= pkg.version %>
+      <%= pkg.description %>
+      <%= pkg.homepage %>
+      Date: <%= moment().format('YYYY-MM-DD') %>
+    `,
+    }),
+  ].filter(plugin => !!plugin)
+
+export default [
+  {
+    input: 'src/index.js',
+    plugins: getPlugins(),
+    output: [
+      {
+        file: 'dist/uploadcare-widget.esm.js',
+        format: 'es',
+      },
+    ],
+  },
+]
