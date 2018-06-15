02762fc7cb17bc42a74083ad9e7f996c3d31b89e
Zarema Khalilova <zarema.khalilova@gmail.com>
2018-06-15 13:34:59 +0300

    Add styles loading to storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,45 @@
+const classPrefix = process.env.npm_package_config_classPrefix
+
+module.exports = (storybookBaseConfig, configType) => {
+  let storybookConfig = {...storybookBaseConfig}
+
+  if (configType === 'PRODUCTION') {
+    // see https://github.com/storybooks/storybook/issues/1570
+    storybookConfig.plugins =
+      [...storybookConfig.plugins.filter(plugin => plugin.constructor.name !== 'UglifyJsPlugin')]
+  }
+
+  return {
+    ...storybookConfig,
+    module: {
+      rules: [
+        ...storybookConfig.module.rules,
+        {
+          test: /\.pcss$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: true,
+                localIdentName: `${classPrefix}[local]`,
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  path: './scripts/postcss.config.js',
+                }
+              }
+            }
+          ]
+        },
+      ],
+    },
+  }
+}
