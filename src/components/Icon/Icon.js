402e6af1078ac6081425caf0a48f8019c6a41093
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2018-06-21 12:46:51 +0300

    Add icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,18 +3,88 @@
 import {h} from 'hyperapp'
 import cn from 'classnames'
 import styles from './Icon.css'
+import * as Icons from './icons'
 
 type Props = {
   className?: string,
+  name: string,
 }
 
-export const Icon = ({className}: Props, children) => (
-  <svg
-    role='presentation'
-    width='32'
-    height='32'
-    class={cn(styles.icon, className)}>
-    {children}
-  </svg>
-)
+export const Icon = ({className, name}: Props) => {
+  let IconComponent
+
+  switch (name) {
+  case 'back':
+    IconComponent = Icons.IconBack
+    break
+  case 'box':
+    IconComponent = Icons.IconBox
+    break
+  case 'camera':
+    IconComponent = Icons.IconCamera
+    break
+  case 'close':
+    IconComponent = Icons.IconClose
+    break
+  case 'crop-free':
+    IconComponent = Icons.IconCropFree
+    break
+  case 'dropbox':
+    IconComponent = Icons.IconDropbox
+    break
+  case 'empty-public-key':
+    IconComponent = Icons.IconEmptyPublicKey
+    break
+  case 'error':
+    IconComponent = Icons.IconError
+    break
+  case 'evernote':
+    IconComponent = Icons.IconEvernote
+    break
+  case 'facebook':
+    IconComponent = Icons.IconFacebook
+    break
+  case 'file':
+    IconComponent = Icons.IconFile
+    break
+  case 'flickr':
+    IconComponent = Icons.IconFlickr
+    break
+  case 'google-drive':
+    IconComponent = Icons.IconGoogleDrive
+    break
+  case 'google-photos':
+    IconComponent = Icons.IconGooglePhotos
+    break
+  case 'huddle':
+    IconComponent = Icons.IconHuddle
+    break
+  case 'instagram':
+    IconComponent = Icons.IconInstagram
+    break
+  case 'menu':
+    IconComponent = Icons.IconMenu
+    break
+  case 'more':
+    IconComponent = Icons.IconMore
+    break
+  case 'remove':
+    IconComponent = Icons.IconRemove
+    break
+  case 'skydrive':
+    IconComponent = Icons.IconSkyDrive
+    break
+  case 'uploadcare':
+    IconComponent = Icons.IconUploadcare
+    break
+  case 'url':
+    IconComponent = Icons.IconUrl
+    break
+  case 'vk':
+    IconComponent = Icons.IconVK
+    break
+  }
+
+  return <IconComponent className={cn(styles.icon, className)} />
+}
 
---
+++ src/components/Icon/icons/index.js
@@ -0,0 +1,49 @@
+import {IconBack} from './IconBack'
+import {IconBox} from './IconBox'
+import {IconCamera} from './IconCamera'
+import {IconClose} from './IconClose'
+import {IconCropFree} from './IconCropFree'
+import {IconDropbox} from './IconDropbox'
+import {IconEmptyPublicKey} from './IconEmptyPublicKey'
+import {IconError} from './IconError'
+import {IconEvernote} from './IconEvernote'
+import {IconFacebook} from './IconFacebook'
+import {IconFile} from './IconFile'
+import {IconFlickr} from './IconFlickr'
+import {IconGoogleDrive} from './IconGoogleDrive'
+import {IconGooglePhotos} from './IconGooglePhotos'
+import {IconHuddle} from './IconHuddle'
+import {IconInstagram} from './IconInstagram'
+import {IconMenu} from './IconMenu'
+import {IconMore} from './IconMore'
+import {IconRemove} from './IconRemove'
+import {IconSkyDrive} from './IconSkyDrive'
+import {IconUploadcare} from './IconUploadcare'
+import {IconUrl} from './IconUrl'
+import {IconVK} from './IconVK'
+
+export {
+  IconBack,
+  IconBox,
+  IconCamera,
+  IconClose,
+  IconCropFree,
+  IconDropbox,
+  IconEmptyPublicKey,
+  IconError,
+  IconEvernote,
+  IconFacebook,
+  IconFile,
+  IconFlickr,
+  IconGoogleDrive,
+  IconGooglePhotos,
+  IconHuddle,
+  IconInstagram,
+  IconMenu,
+  IconMore,
+  IconRemove,
+  IconSkyDrive,
+  IconUploadcare,
+  IconUrl,
+  IconVK,
+}
