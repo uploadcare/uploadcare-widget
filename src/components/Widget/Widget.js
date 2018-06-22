eb2508f0486b37b6443046559707c5a35ad82c9e
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2018-06-22 14:19:10 +0300

    Update Widget

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -4,10 +4,16 @@ import {h} from 'hyperapp'
 import cn from 'classnames'
 import styles from './Widget.css'
 
+import {translate} from '../../helpers'
+
 import type {Props} from './flow-typed'
 
-export const Widget = ({className}: Props, children: Children) => (
+export const Widget = ({className}: Props) => (
   <div className={cn(styles.widget, className)}>
-    {children}
+    <div class={styles['widget__dragndrop-area']}>
+      {translate('draghere')}
+    </div>
+    <div class={styles.widget__progress} />
+    <div class={styles.widget__text} />
   </div>
 )
