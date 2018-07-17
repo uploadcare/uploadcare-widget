760e6d812d9ef959f333878c714496dfcdeded54
Александр Гренишин <nd0ut.me@gmail.com>
2018-07-17 18:14:14 +0300

    add spec for function

--- src/modules/settings/normalize/cast/func.js
+++ src/modules/settings/normalize/cast/func.js
@@ -1,12 +1,12 @@
 /* @flow */
 
+import {SettingsError} from 'errors/SettingsError'
+
 import type {ValueTransformer} from '../flow-typed/ValueTransformer'
 
 export const func: ValueTransformer<?Function> = (value: any) => {
   if (typeof value !== 'function') {
-    console.warn('Not a function')
-
-    return null
+    throw new SettingsError('Not a function', null)
   }
 
   return value
