73254924bf792f0e1e4fb2b362ec6574498add81
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2018-07-19 11:11:23 +0300

    Update files structure

--- scripts/tests/e2e/utils/run.js
+++ scripts/tests/e2e/utils/run.js
@@ -1,7 +1,7 @@
 const {exec} = require('child_process')
 
 /* eslint-disable no-console */
-const run = (browsers = 'chrome:headless', path = '__tests__/e2e/**/*.test.js') =>
+const run = (browsers = 'chrome:headless', path = 'tests/e2e/**/*.test.js') =>
   exec(`testcafe "${browsers}" ${path}`, (error, stdout) => {
     if (error) {
       console.error(error)
