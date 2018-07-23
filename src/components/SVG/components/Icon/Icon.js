e628aa578f53adf28dc39eb6a7342b49ba2fb5a0
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2018-07-23 12:34:23 +0300

    Replace switch in icons with UpperCamelCase function

--- src/components/SVG/components/Icon/Icon.js
+++ src/components/SVG/components/Icon/Icon.js
@@ -7,80 +7,17 @@ import * as Icons from './icons/index'
 
 import type {Props} from './flow-typed'
 
-export const Icon = ({className, name}: Props) => {
-  let IconComponent
+/**
+ * Convert kebab-case to UpperCamelCase.
+ * @param {string} key
+ * @return {string}
+ */
+const kebab2upperCamelCase = (key: string): string => key.split('-')
+  .map(item => item.replace(/^\w/, character => character.toUpperCase()))
+  .join('')
 
-  switch (name) {
-  case 'back':
-    IconComponent = Icons.IconBack
-    break
-  case 'box':
-    IconComponent = Icons.IconBox
-    break
-  case 'camera':
-    IconComponent = Icons.IconCamera
-    break
-  case 'close':
-    IconComponent = Icons.IconClose
-    break
-  case 'crop-free':
-    IconComponent = Icons.IconCropFree
-    break
-  case 'dropbox':
-    IconComponent = Icons.IconDropbox
-    break
-  case 'empty-public-key':
-    IconComponent = Icons.IconEmptyPublicKey
-    break
-  case 'error':
-    IconComponent = Icons.IconError
-    break
-  case 'evernote':
-    IconComponent = Icons.IconEvernote
-    break
-  case 'facebook':
-    IconComponent = Icons.IconFacebook
-    break
-  case 'file':
-    IconComponent = Icons.IconFile
-    break
-  case 'flickr':
-    IconComponent = Icons.IconFlickr
-    break
-  case 'google-drive':
-    IconComponent = Icons.IconGoogleDrive
-    break
-  case 'google-photos':
-    IconComponent = Icons.IconGooglePhotos
-    break
-  case 'huddle':
-    IconComponent = Icons.IconHuddle
-    break
-  case 'instagram':
-    IconComponent = Icons.IconInstagram
-    break
-  case 'menu':
-    IconComponent = Icons.IconMenu
-    break
-  case 'more':
-    IconComponent = Icons.IconMore
-    break
-  case 'remove':
-    IconComponent = Icons.IconRemove
-    break
-  case 'skydrive':
-    IconComponent = Icons.IconSkyDrive
-    break
-  case 'uploadcare':
-    IconComponent = Icons.IconUploadcare
-    break
-  case 'url':
-    IconComponent = Icons.IconUrl
-    break
-  case 'vk':
-    IconComponent = Icons.IconVK
-    break
-  }
+export const Icon = ({className, name}: Props) => {
+  const IconComponent = Icons[`Icon${kebab2upperCamelCase(name)}`]
 
   return <IconComponent className={cn(styles.icon, className)} />
 }
