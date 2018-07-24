dbce07a19911951aeecb1bbbe7bb23b23b17a947
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2018-07-24 12:52:06 +0300

    Remove `kebab2upperCamelCase` to helpers

--- src/components/SVG/components/Icon/Icon.js
+++ src/components/SVG/components/Icon/Icon.js
@@ -4,17 +4,9 @@ import {h} from 'hyperapp'
 import cn from 'classnames'
 import styles from './Icon.css'
 import * as Icons from './icons/index'
+import {kebab2upperCamelCase} from '../../../../helpers/case'
 
 import type {Props} from './flow-typed'
-
-/**
- * Convert kebab-case to UpperCamelCase.
- * @param {string} key
- * @return {string}
- */
-const kebab2upperCamelCase = (key: string): string => key.split('-')
-  .map(item => item.replace(/^\w/, character => character.toUpperCase()))
-  .join('')
 
 export const Icon = ({className, name}: Props) => {
   const IconComponent = Icons[`Icon${kebab2upperCamelCase(name)}`]
--- src/helpers/case/case.js
+++ src/helpers/case/case.js
@@ -21,3 +21,12 @@ export function camel2upper(str: string): string {
 export function camel2kebab(str: string): string {
   return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
 }
+
+/**
+ * Convert kebab-case to UpperCamelCase.
+ * @param {string} key
+ * @return {string}
+ */
+export const kebab2upperCamelCase = (key: string): string => key.split('-')
+  .map(item => item.replace(/^\w/, character => character.toUpperCase()))
+  .join('')
