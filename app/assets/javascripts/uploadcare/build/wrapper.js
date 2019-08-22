a723b44611e580e4512149de0d2594f814079057
Ivakhnenko Dmitry <jeetiss@yandex.ru>
2019-08-22 11:26:28 +0300

    chore(lint): add standard eslint config (#552)

--- app/assets/javascripts/uploadcare/build/wrapper.js
+++ app/assets/javascripts/uploadcare/build/wrapper.js
@@ -1,19 +1,21 @@
-;(function(global, factory) {
+(function (global, factory) {
   // Not a browser enviroment at all: not Browserify/Webpack.
-  if ( ! global.document) {
-    return;
+  if (!global.document) {
+    return
   }
 
-  if (typeof module === "object" && module.exports) {
-    module.exports = factory(global, require("jquery"));
+  if (typeof module === 'object' && module.exports) {
+    module.exports = factory(global, require('jquery'))
   } else {
-    global.uploadcare = factory(global);
+    global.uploadcare = factory(global)
   }
+}(typeof window !== 'undefined' ? window : this, function (window, jQuery) {
+  var uploadcare
+  // eslint-disable-next-line no-unused-vars
+  var document = window.document
 
-}(typeof window !== "undefined" ? window : this, function(window, jQuery) {
-  var uploadcare, document = window.document;
+  // eslint-disable-next-line no-undef, no-unused-expressions, camelcase
+  ___widget_code___
 
-___widget_code___
-
-  return uploadcare.__exports;
-}));
+  return uploadcare.__exports
+}))
