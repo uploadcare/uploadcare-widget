0c772a209a96c224733f6be503bde4a6a1215b03
jeetiss <jeetiss@yandex.ru>
2019-08-26 13:57:45 +0300

    init e2e tests with cypress

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+/* eslint-env mocha */
+
+describe('uploadcare widget', function () {
+  beforeEach(function () {
+    cy.visit('/')
+  })
+
+  it('uploads with url', function () {
+    cy.get('.uploadcare--progress').should('not.exist')
+    cy.get('.uploadcare--widget__button_type_open').click()
+
+    cy.get('.uploadcare--menu__item_tab_url').click()
+    cy.get('.uploadcare--input').type('https://ucarecdn.com/3db26848-d1c6-4b3d-8cfa-13cc5a76f2a1/small.png{enter}')
+
+    cy.get('.uploadcare--progress').should('exist')
+
+    cy.get('.uploadcare--link').should('exist')
+  })
+})
