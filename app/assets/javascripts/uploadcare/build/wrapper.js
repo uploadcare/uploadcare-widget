dd051e33a38ec2107658c6fef5b429c792bc4e2c
jeetiss <jeetiss@yandex.ru>
2019-09-27 13:31:59 +0300

    revert wrapper js

--- app/assets/javascripts/uploadcare/build/wrapper.js
+++ app/assets/javascripts/uploadcare/build/wrapper.js
@@ -1,21 +1,19 @@
-(function (global, factory) {
+;(function(global, factory) {
   // Not a browser enviroment at all: not Browserify/Webpack.
-  if (!global.document) {
-    return
+  if ( ! global.document) {
+    return;
   }
 
-  if (typeof module === 'object' && module.exports) {
-    module.exports = factory(global, require('jquery'))
+  if (typeof module === "object" && module.exports) {
+    module.exports = factory(global, require("jquery"));
   } else {
-    global.uploadcare = factory(global)
+    global.uploadcare = factory(global);
   }
-}(typeof window !== 'undefined' ? window : this, function (window, jQuery) {
-  var uploadcare
-  // eslint-disable-next-line no-unused-vars, semi
-  var document = window.document;
 
-  // eslint-disable-next-line no-undef, no-unused-expressions, camelcase
-  ___widget_code___
+}(typeof window !== "undefined" ? window : this, function(window, jQuery) {
+  var uploadcare, document = window.document;
 
-  return uploadcare.__exports
-}))
+___widget_code___
+
+  return uploadcare.__exports;
+}));
