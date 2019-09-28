13d761f92db87b3128bc046f1459e06428cdbeec
jeetiss <jeetiss@yandex.ru>
2019-09-28 15:08:51 +0300

    locales as module

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,174 +1,171 @@
-import uploadcare from '../namespace'
 
 // #
 // # English locale is the default and used as a fallback.
 // #
-uploadcare.namespace('locale.translations', function (ns) {
-  ns.en = {
-    uploading: 'Uploading... Please wait.',
-    loadingInfo: 'Loading info...',
-    errors: {
-      default: 'Error',
-      baddata: 'Incorrect value',
-      size: 'File too big',
-      upload: 'Can’t upload',
-      user: 'Upload canceled',
-      info: 'Can’t load info',
-      image: 'Only images allowed',
-      createGroup: 'Can’t create file group',
-      deleted: 'File was deleted'
-    },
-    draghere: 'Drop a file here',
-    file: {
-      one: '%1 file',
-      other: '%1 files'
-    },
-    buttons: {
-      cancel: 'Cancel',
-      remove: 'Remove',
-      choose: {
-        files: {
-          one: 'Choose a file',
-          other: 'Choose files'
-        },
-        images: {
-          one: 'Choose an image',
-          other: 'Choose images'
-        }
-      }
-    },
-    dialog: {
-      close: 'Close',
-      openMenu: 'Open menu',
-      done: 'Done',
-      showFiles: 'Show files',
-      tabs: {
-        names: {
-          'empty-pubkey': 'Welcome',
-          preview: 'Preview',
-          file: 'Local Files',
-          url: 'Direct Link',
-          camera: 'Camera',
-          facebook: 'Facebook',
-          dropbox: 'Dropbox',
-          gdrive: 'Google Drive',
-          gphotos: 'Google Photos',
-          instagram: 'Instagram',
-          vk: 'VK',
-          evernote: 'Evernote',
-          box: 'Box',
-          onedrive: 'OneDrive',
-          flickr: 'Flickr',
-          huddle: 'Huddle'
-        },
-        file: {
-          drag: 'drag & drop<br>any files',
-          nodrop: 'Upload files from your&nbsp;computer',
-          cloudsTip: 'Cloud storages<br>and social networks',
-          or: 'or',
-          button: 'Choose a local file',
-          also: 'or choose from'
-        },
-        url: {
-          title: 'Files from the Web',
-          line1: 'Grab any file off the web.',
-          line2: 'Just provide the link.',
-          input: 'Paste your link here...',
-          button: 'Upload'
-        },
-        camera: {
-          title: 'File from web camera',
-          capture: 'Take a photo',
-          mirror: 'Mirror',
-          startRecord: 'Record a video',
-          stopRecord: 'Stop',
-          cancelRecord: 'Cancel',
-          retry: 'Request permissions again',
-          pleaseAllow: {
-            title: 'Please allow access to your camera',
-            text: 'You have been prompted to allow camera access from this site.<br>' + 'In order to take pictures with your camera you must approve this request.'
-          },
-          notFound: {
-            title: 'No camera detected',
-            text: 'Looks like you have no camera connected to this device.'
-          }
-        },
-        preview: {
-          unknownName: 'unknown',
-          change: 'Cancel',
-          back: 'Back',
-          done: 'Add',
-          unknown: {
-            title: 'Uploading... Please wait for a preview.',
-            done: 'Skip preview and accept'
-          },
-          regular: {
-            title: 'Add this file?',
-            line1: 'You are about to add the file above.',
-            line2: 'Please confirm.'
-          },
-          image: {
-            title: 'Add this image?',
-            change: 'Cancel'
-          },
-          crop: {
-            title: 'Crop and add this image',
-            done: 'Done',
-            free: 'free'
-          },
-          video: {
-            title: 'Add this video?',
-            change: 'Cancel'
-          },
-          error: {
-            default: {
-              title: 'Oops!',
-              text: 'Something went wrong during the upload.',
-              back: 'Please try again'
-            },
-            image: {
-              title: 'Only image files are accepted.',
-              text: 'Please try again with another file.',
-              back: 'Choose image'
-            },
-            size: {
-              title: 'The file you selected exceeds the limit.',
-              text: 'Please try again with another file.'
-            },
-            loadImage: {
-              title: 'Error',
-              text: 'Can’t load image'
-            }
-          },
-          multiple: {
-            title: 'You’ve chosen %files%.',
-            question: 'Add %files%?',
-            tooManyFiles: 'You’ve chosen too many files. %max% is maximum.',
-            tooFewFiles: 'You’ve chosen %files%. At least %min% required.',
-            clear: 'Remove all',
-            done: 'Add',
-            file: {
-              preview: 'Preview %file%',
-              remove: 'Remove %file%'
-            }
-          }
-        }
+const translate = {
+  uploading: 'Uploading... Please wait.',
+  loadingInfo: 'Loading info...',
+  errors: {
+    default: 'Error',
+    baddata: 'Incorrect value',
+    size: 'File too big',
+    upload: 'Can’t upload',
+    user: 'Upload canceled',
+    info: 'Can’t load info',
+    image: 'Only images allowed',
+    createGroup: 'Can’t create file group',
+    deleted: 'File was deleted'
+  },
+  draghere: 'Drop a file here',
+  file: {
+    one: '%1 file',
+    other: '%1 files'
+  },
+  buttons: {
+    cancel: 'Cancel',
+    remove: 'Remove',
+    choose: {
+      files: {
+        one: 'Choose a file',
+        other: 'Choose files'
       },
-      footer: {
-        text: 'powered by',
-        link: 'uploadcare'
+      images: {
+        one: 'Choose an image',
+        other: 'Choose images'
       }
     }
+  },
+  dialog: {
+    close: 'Close',
+    openMenu: 'Open menu',
+    done: 'Done',
+    showFiles: 'Show files',
+    tabs: {
+      names: {
+        'empty-pubkey': 'Welcome',
+        preview: 'Preview',
+        file: 'Local Files',
+        url: 'Direct Link',
+        camera: 'Camera',
+        facebook: 'Facebook',
+        dropbox: 'Dropbox',
+        gdrive: 'Google Drive',
+        gphotos: 'Google Photos',
+        instagram: 'Instagram',
+        vk: 'VK',
+        evernote: 'Evernote',
+        box: 'Box',
+        onedrive: 'OneDrive',
+        flickr: 'Flickr',
+        huddle: 'Huddle'
+      },
+      file: {
+        drag: 'drag & drop<br>any files',
+        nodrop: 'Upload files from your&nbsp;computer',
+        cloudsTip: 'Cloud storages<br>and social networks',
+        or: 'or',
+        button: 'Choose a local file',
+        also: 'or choose from'
+      },
+      url: {
+        title: 'Files from the Web',
+        line1: 'Grab any file off the web.',
+        line2: 'Just provide the link.',
+        input: 'Paste your link here...',
+        button: 'Upload'
+      },
+      camera: {
+        title: 'File from web camera',
+        capture: 'Take a photo',
+        mirror: 'Mirror',
+        startRecord: 'Record a video',
+        stopRecord: 'Stop',
+        cancelRecord: 'Cancel',
+        retry: 'Request permissions again',
+        pleaseAllow: {
+          title: 'Please allow access to your camera',
+          text: 'You have been prompted to allow camera access from this site.<br>' + 'In order to take pictures with your camera you must approve this request.'
+        },
+        notFound: {
+          title: 'No camera detected',
+          text: 'Looks like you have no camera connected to this device.'
+        }
+      },
+      preview: {
+        unknownName: 'unknown',
+        change: 'Cancel',
+        back: 'Back',
+        done: 'Add',
+        unknown: {
+          title: 'Uploading... Please wait for a preview.',
+          done: 'Skip preview and accept'
+        },
+        regular: {
+          title: 'Add this file?',
+          line1: 'You are about to add the file above.',
+          line2: 'Please confirm.'
+        },
+        image: {
+          title: 'Add this image?',
+          change: 'Cancel'
+        },
+        crop: {
+          title: 'Crop and add this image',
+          done: 'Done',
+          free: 'free'
+        },
+        video: {
+          title: 'Add this video?',
+          change: 'Cancel'
+        },
+        error: {
+          default: {
+            title: 'Oops!',
+            text: 'Something went wrong during the upload.',
+            back: 'Please try again'
+          },
+          image: {
+            title: 'Only image files are accepted.',
+            text: 'Please try again with another file.',
+            back: 'Choose image'
+          },
+          size: {
+            title: 'The file you selected exceeds the limit.',
+            text: 'Please try again with another file.'
+          },
+          loadImage: {
+            title: 'Error',
+            text: 'Can’t load image'
+          }
+        },
+        multiple: {
+          title: 'You’ve chosen %files%.',
+          question: 'Add %files%?',
+          tooManyFiles: 'You’ve chosen too many files. %max% is maximum.',
+          tooFewFiles: 'You’ve chosen %files%. At least %min% required.',
+          clear: 'Remove all',
+          done: 'Add',
+          file: {
+            preview: 'Preview %file%',
+            remove: 'Remove %file%'
+          }
+        }
+      }
+    },
+    footer: {
+      text: 'powered by',
+      link: 'uploadcare'
+    }
   }
-})
+}
 
 // Pluralization rules taken from:
 // http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
-uploadcare.namespace('locale.pluralize', function (ns) {
-  ns.en = function (n) {
-    if (n === 1) {
-      return 'one'
-    }
-    return 'other'
+const pluralize = function (n) {
+  if (n === 1) {
+    return 'one'
   }
-})
+  return 'other'
+}
+
+export { pluralize, translate }
--- src/locales/uk.js
+++ src/locales/uk.js
@@ -1,4 +1,3 @@
-import uploadcare from '../namespace'
 
 // #
 // # Please, do not use this locale as a reference for new translations.
@@ -7,176 +6,174 @@ import uploadcare from '../namespace'
 // #
 // # Any fixes are welcome.
 // #
-uploadcare.namespace('locale.translations', function (ns) {
-  ns.uk = {
-    uploading: 'Завантаження... Зачекайте.',
-    loadingInfo: 'Завантаження інформації...',
-    errors: {
-      default: 'Помилка',
-      baddata: 'Неправильне значення',
-      size: 'Завеликий файл',
-      upload: 'Помилка завантаження',
-      user: 'Завантаження скасовано',
-      info: 'Помилка завантаження інформації',
-      image: 'Дозволені лише зображення',
-      createGroup: 'Неможливо створити групу файлів',
-      deleted: 'Файл видалено'
-    },
-    draghere: 'Перетягніть файл сюди',
-    file: {
-      one: '%1 файл',
-      few: '%1 файли',
-      many: '%1 файлів'
-    },
-    buttons: {
-      cancel: 'Cкасувати',
-      remove: 'Видалити',
-      choose: {
-        files: {
-          one: 'Вибрати файл',
-          other: 'Вибрати файли'
-        },
-        images: {
-          one: 'Вибрати зображення',
-          other: 'Вибрати зображення'
-        }
-      }
-    },
-    dialog: {
-      close: 'Закрити',
-      openMenu: 'Відкрити меню',
-      done: 'Готово',
-      showFiles: 'Показати файли',
-      tabs: {
-        names: {
-          'empty-pubkey': 'Вітання',
-          preview: 'Попередній перегляд',
-          file: 'Локальні файли',
-          url: 'Пряме посилання',
-          camera: 'Камера',
-          facebook: 'Facebook',
-          dropbox: 'Dropbox',
-          gdrive: 'Google Drive',
-          gphotos: 'Google Photos',
-          instagram: 'Instagram',
-          vk: 'VK',
-          evernote: 'Evernote',
-          box: 'Box',
-          onedrive: 'OneDrive',
-          flickr: 'Flickr',
-          huddle: 'Huddle'
-        },
-        file: {
-          drag: 'Перетягніть<br>будь-які файли',
-          nodrop: 'Завантаження файлів з вашого комп\'ютера',
-          cloudsTip: 'Хмарні сховища<br>та соціальні мережі',
-          or: 'або',
-          button: 'Обрати локальний файл',
-          also: 'або обрати з'
-        },
-        url: {
-          title: 'Файли з інших сайтів',
-          line1: 'Візьміть будь-який файл з Інтернету..',
-          line2: 'Вкажіть тут посилання.',
-          input: 'Вставте ваше посилання тут...',
-          button: 'Завантажити'
-        },
-        camera: {
-          title: 'Файл із відеокамери',
-          capture: 'Зробити знімок',
-          mirror: 'Віддзеркалити',
-          startRecord: 'Записати відео',
-          stopRecord: 'Стоп',
-          cancelRecord: 'Cкасувати',
-          retry: 'Повторний запит дозволу',
-          pleaseAllow: {
-            title: 'Будь ласка, надайте доступ до вашої камери',
-            text: 'Вам буде запропоновано дозволити доступ до камери з цього сайту.<br>' + 'Для того, щоб фотографувати за допомогою камери, ви повинні схвалити цей запит.'
-          },
-          notFound: {
-            title: 'Камера не виявлена',
-            text: 'Схоже, у вас немає камери, підключеної до цього пристрою.'
-          }
-        },
-        preview: {
-          unknownName: 'невідомо',
-          change: 'Cкасувати',
-          back: 'Назад',
-          done: 'Додати'
-        },
-        unknown: {
-          title: 'Завантаження... Зачекайте на попередній перегляд.',
-          done: 'Пропустити перегляд і прийняти'
-        },
-        regular: {
-          title: 'Додати цей файл?',
-          line1: 'Ви збираєтеся додати файл вище.',
-          line2: 'Будь ласка, підтвердіть.'
-        },
-        image: {
-          title: 'Додати це зображення?',
-          change: 'Cкасувати'
-        },
-        crop: {
-          title: 'Обрізати та додати це зображення',
-          done: 'Готово',
-          free: 'довільно'
-        },
-        video: {
-          title: 'Додати це відео?',
-          change: 'Cкасувати'
-        },
-        error: {
-          default: {
-            title: 'Ой!',
-            text: 'Під час завантаження сталася помилка.',
-            back: 'Будь ласка, спробуйте ще раз'
-          },
-          image: {
-            title: 'Приймаються лише файли зображень.',
-            text: 'Повторіть спробу з іншим файлом.',
-            back: 'Виберіть зображення'
-          },
-          size: {
-            title: 'Розмір вибраного файлу перевищує ліміт.',
-            text: 'Повторіть спробу з іншим файлом.'
-          },
-          loadImage: {
-            title: 'Помилка',
-            text: 'Помилка завантаження зображення'
-          },
-          multiple: {
-            title: 'Ви вибрали %files%.',
-            question: 'Додати %files%?',
-            tooManyFiles: 'Ви вибрали забагато файлів. Максимальна кількість %max%.',
-            tooFewFiles: 'Ви вибрали %files%. Мінімальна кількість %min%.',
-            clear: 'Видалити все',
-            done: 'Додати'
-          },
-          file: {
-            preview: 'Попередній перегляд %file%',
-            remove: 'Видалити %file%'
-          }
-        }
+const translate = {
+  uploading: 'Завантаження... Зачекайте.',
+  loadingInfo: 'Завантаження інформації...',
+  errors: {
+    default: 'Помилка',
+    baddata: 'Неправильне значення',
+    size: 'Завеликий файл',
+    upload: 'Помилка завантаження',
+    user: 'Завантаження скасовано',
+    info: 'Помилка завантаження інформації',
+    image: 'Дозволені лише зображення',
+    createGroup: 'Неможливо створити групу файлів',
+    deleted: 'Файл видалено'
+  },
+  draghere: 'Перетягніть файл сюди',
+  file: {
+    one: '%1 файл',
+    few: '%1 файли',
+    many: '%1 файлів'
+  },
+  buttons: {
+    cancel: 'Cкасувати',
+    remove: 'Видалити',
+    choose: {
+      files: {
+        one: 'Вибрати файл',
+        other: 'Вибрати файли'
       },
-      footer: {
-        text: 'працює на',
-        link: 'uploadcare'
+      images: {
+        one: 'Вибрати зображення',
+        other: 'Вибрати зображення'
       }
     }
+  },
+  dialog: {
+    close: 'Закрити',
+    openMenu: 'Відкрити меню',
+    done: 'Готово',
+    showFiles: 'Показати файли',
+    tabs: {
+      names: {
+        'empty-pubkey': 'Вітання',
+        preview: 'Попередній перегляд',
+        file: 'Локальні файли',
+        url: 'Пряме посилання',
+        camera: 'Камера',
+        facebook: 'Facebook',
+        dropbox: 'Dropbox',
+        gdrive: 'Google Drive',
+        gphotos: 'Google Photos',
+        instagram: 'Instagram',
+        vk: 'VK',
+        evernote: 'Evernote',
+        box: 'Box',
+        onedrive: 'OneDrive',
+        flickr: 'Flickr',
+        huddle: 'Huddle'
+      },
+      file: {
+        drag: 'Перетягніть<br>будь-які файли',
+        nodrop: 'Завантаження файлів з вашого комп\'ютера',
+        cloudsTip: 'Хмарні сховища<br>та соціальні мережі',
+        or: 'або',
+        button: 'Обрати локальний файл',
+        also: 'або обрати з'
+      },
+      url: {
+        title: 'Файли з інших сайтів',
+        line1: 'Візьміть будь-який файл з Інтернету..',
+        line2: 'Вкажіть тут посилання.',
+        input: 'Вставте ваше посилання тут...',
+        button: 'Завантажити'
+      },
+      camera: {
+        title: 'Файл із відеокамери',
+        capture: 'Зробити знімок',
+        mirror: 'Віддзеркалити',
+        startRecord: 'Записати відео',
+        stopRecord: 'Стоп',
+        cancelRecord: 'Cкасувати',
+        retry: 'Повторний запит дозволу',
+        pleaseAllow: {
+          title: 'Будь ласка, надайте доступ до вашої камери',
+          text: 'Вам буде запропоновано дозволити доступ до камери з цього сайту.<br>' + 'Для того, щоб фотографувати за допомогою камери, ви повинні схвалити цей запит.'
+        },
+        notFound: {
+          title: 'Камера не виявлена',
+          text: 'Схоже, у вас немає камери, підключеної до цього пристрою.'
+        }
+      },
+      preview: {
+        unknownName: 'невідомо',
+        change: 'Cкасувати',
+        back: 'Назад',
+        done: 'Додати'
+      },
+      unknown: {
+        title: 'Завантаження... Зачекайте на попередній перегляд.',
+        done: 'Пропустити перегляд і прийняти'
+      },
+      regular: {
+        title: 'Додати цей файл?',
+        line1: 'Ви збираєтеся додати файл вище.',
+        line2: 'Будь ласка, підтвердіть.'
+      },
+      image: {
+        title: 'Додати це зображення?',
+        change: 'Cкасувати'
+      },
+      crop: {
+        title: 'Обрізати та додати це зображення',
+        done: 'Готово',
+        free: 'довільно'
+      },
+      video: {
+        title: 'Додати це відео?',
+        change: 'Cкасувати'
+      },
+      error: {
+        default: {
+          title: 'Ой!',
+          text: 'Під час завантаження сталася помилка.',
+          back: 'Будь ласка, спробуйте ще раз'
+        },
+        image: {
+          title: 'Приймаються лише файли зображень.',
+          text: 'Повторіть спробу з іншим файлом.',
+          back: 'Виберіть зображення'
+        },
+        size: {
+          title: 'Розмір вибраного файлу перевищує ліміт.',
+          text: 'Повторіть спробу з іншим файлом.'
+        },
+        loadImage: {
+          title: 'Помилка',
+          text: 'Помилка завантаження зображення'
+        },
+        multiple: {
+          title: 'Ви вибрали %files%.',
+          question: 'Додати %files%?',
+          tooManyFiles: 'Ви вибрали забагато файлів. Максимальна кількість %max%.',
+          tooFewFiles: 'Ви вибрали %files%. Мінімальна кількість %min%.',
+          clear: 'Видалити все',
+          done: 'Додати'
+        },
+        file: {
+          preview: 'Попередній перегляд %file%',
+          remove: 'Видалити %file%'
+        }
+      }
+    },
+    footer: {
+      text: 'працює на',
+      link: 'uploadcare'
+    }
   }
-})
+}
 
 // Pluralization rules taken from:
 // http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
-uploadcare.namespace('locale.pluralize', function (ns) {
-  ns.uk = function (n) {
-    if (((n / 10 % 10 | 0) === 1) || (n % 10 === 0) || (n % 10 > 4)) {
-      return 'many'
-    } else if (n % 10 === 1) {
-      return 'one'
-    } else {
-      return 'few'
-    }
+const pluralize = function (n) {
+  if (((n / 10 % 10 | 0) === 1) || (n % 10 === 0) || (n % 10 > 4)) {
+    return 'many'
+  } else if (n % 10 === 1) {
+    return 'one'
+  } else {
+    return 'few'
   }
-})
+}
+
+export { translate, pluralize }
