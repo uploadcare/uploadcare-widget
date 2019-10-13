5d8d01a82e6740a02ff15363b279b1a0f34da615
jeetiss <jeetiss@yandex.ru>
2019-10-13 20:17:52 +0300

    add comment

--- src/utils/html.js
+++ src/utils/html.js
@@ -1,3 +1,8 @@
+// This module used only in dev mod
+
+// For production 'babel-plugin-html-tag' removes html
+// function and evaluates and minifies tagged html
+
 const html = (parts, ...values) =>
   parts.reduce(
     (str, next, i) => str + values[i - 1] + next
