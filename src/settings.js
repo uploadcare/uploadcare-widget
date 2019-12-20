40313ff60a032f4987d0ff26fe2330e0a9708c1c
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-12-20 14:09:02 +0300

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ import { version } from '../package.json'
 
 import { sendFileAPI } from './utils/abilities'
 import { warnOnce } from './utils/warnings'
-import { unique, once, upperCase, normalizeUrl } from './utils'
+import { unique, once, upperCase, normalizeUrl, isFunction } from './utils'
 import { isWindowDefined } from './utils/is-window-defined'
 
 var indexOf = [].indexOf
@@ -362,7 +362,213 @@ const build = function(settings) {
   return result
 }
 
-const waitForSettings = isWindowDefined() && $.Callbacks('once memory')
+// Convert String-formatted options into Object-formatted ones
+const createOptions = options => {
+  var rnothtmlwhite = /[^\x20\t\r\n\f]+/g
+  var object = {}
+  var arr = options.match(rnothtmlwhite) || []
+  arr.forEach(function(_, flag) {
+    object[flag] = true
+  })
+  return object
+}
+
+const toType = obj => {
+  if (obj == null) {
+    return obj + ''
+  }
+  var class2type = {}
+
+  // Support: Android <=2.3 only (functionish RegExp)
+  return typeof obj === 'object' || typeof obj === 'function'
+    ? class2type[toString.call(obj)] || 'object'
+    : typeof obj
+}
+
+const inArray = (elem, arr, i) => {
+  return arr == null ? -1 : indexOf.call(arr, elem, i)
+}
+
+const callbacks = function(options) {
+  // Convert options from String-formatted to Object-formatted if needed
+  // (we check in cache first)
+  options =
+    typeof options === 'string'
+      ? createOptions(options)
+      : Object.assign({}, options)
+
+  var firing
+  // Last fire value for non-forgettable lists
+  var memory
+  // Flag to know if list was already fired
+  var fired
+  // Flag to prevent firing
+  var locked
+  // Actual callback list
+  var list = []
+  // Queue of execution data for repeatable lists
+  var queue = []
+  // Index of currently firing callback (modified by add/remove as needed)
+  var firingIndex = -1
+  // Fire callbacks
+  var fire = function() {
+    // Enforce single-firing
+    locked = locked || options.once
+
+    // Execute callbacks for all pending executions,
+    // respecting firingIndex overrides and runtime changes
+    fired = firing = true
+    for (; queue.length; firingIndex = -1) {
+      memory = queue.shift()
+      while (++firingIndex < list.length) {
+        // Run callback and check for early termination
+        if (
+          list[firingIndex].apply(memory[0], memory[1]) === false &&
+          options.stopOnFalse
+        ) {
+          // Jump to end and forget the data so .add doesn't re-fire
+          firingIndex = list.length
+          memory = false
+        }
+      }
+    }
+
+    // Forget the data if we're done with it
+    if (!options.memory) {
+      memory = false
+    }
+
+    firing = false
+
+    // Clean up if we're done firing for good
+    if (locked) {
+      // Keep an empty list if we have data for future add calls
+      if (memory) {
+        list = []
+
+        // Otherwise, this object is spent
+      } else {
+        list = ''
+      }
+    }
+  }
+  // Actual Callbacks object
+  var self = {
+    // Add a callback or a collection of callbacks to the list
+    add: function() {
+      if (list) {
+        // If we have memory from a past run, we should fire after adding
+        if (memory && !firing) {
+          firingIndex = list.length - 1
+          queue.push(memory)
+        }
+
+        ;(function add(args) {
+          args.forEach(function(_, arg) {
+            if (isFunction(arg)) {
+              if (!options.unique || !self.has(arg)) {
+                list.push(arg)
+              }
+            } else if (arg && arg.length && toType(arg) !== 'string') {
+              // Inspect recursively
+              add(arg)
+            }
+          })
+        })(arguments)
+
+        if (memory && !firing) {
+          fire()
+        }
+      }
+      return this
+    },
+
+    // Remove a callback from the list
+    remove: function() {
+      arguments.forEach(function(_, arg) {
+        var index
+        while ((index = inArray(arg, list, index)) > -1) {
+          // while ((index = jQuery.inArray(arg, list, index)) > -1) {
+          list.splice(index, 1)
+
+          // Handle firing indexes
+          if (index <= firingIndex) {
+            firingIndex--
+          }
+        }
+      })
+      return this
+    },
+
+    // Check if a given callback is in the list.
+    // If no argument is given, return whether or not list has callbacks attached.
+    has: function(fn) {
+      return fn ? inArray(fn, list) > -1 : list.length > 0
+    },
+
+    // Remove all callbacks from the list
+    empty: function() {
+      if (list) {
+        list = []
+      }
+      return this
+    },
+
+    // Disable .fire and .add
+    // Abort any current/pending executions
+    // Clear all callbacks and values
+    disable: function() {
+      locked = queue = []
+      list = memory = ''
+      return this
+    },
+    disabled: function() {
+      return !list
+    },
+
+    // Disable .fire
+    // Also disable .add unless we have memory (since it would have no effect)
+    // Abort any pending executions
+    lock: function() {
+      locked = queue = []
+      if (!memory && !firing) {
+        list = memory = ''
+      }
+      return this
+    },
+    locked: function() {
+      return !!locked
+    },
+
+    // Call all callbacks with the given context and arguments
+    fireWith: function(context, args) {
+      if (!locked) {
+        args = args || []
+        args = [context, args.slice ? args.slice() : args]
+        queue.push(args)
+        if (!firing) {
+          fire()
+        }
+      }
+      return this
+    },
+
+    // Call all the callbacks with the given arguments
+    fire: function() {
+      self.fireWith(this, arguments)
+      return this
+    },
+
+    // To know if the callbacks have already been called at least once
+    fired: function() {
+      return !!fired
+    }
+  }
+
+  return self
+}
+
+const waitForSettings = isWindowDefined() && callbacks('once memory')
 
 const CssCollector = class CssCollector {
   constructor() {
@@ -384,7 +590,7 @@ const CssCollector = class CssCollector {
   }
 }
 
-const isPlainObject = (obj) => {
+const isPlainObject = obj => {
   return Object.prototype.toString.call(obj) === '[object Object]'
 }
 
