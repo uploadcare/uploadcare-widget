277ff1afb9bb6ba6b034f1b67647703f8994f5c1
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-12-20 13:20:21 +0300

    Remove $ from messages.js

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,3 @@
-import $ from 'jquery'
 import { isWindowDefined } from './is-window-defined'
 
 // utils
@@ -6,7 +5,7 @@ import { isWindowDefined } from './is-window-defined'
 var callbacks = {}
 
 isWindowDefined() &&
-  $(window).on('message', ({ originalEvent: e }) => {
+  window.addEventListener('message', ({ originalEvent: e }) => {
     var i, item, len, message, ref, results
     try {
       message = JSON.parse(e.data)
@@ -39,9 +38,28 @@ const registerMessage = function(type, sender, callback) {
   return callbacks[type].push([sender, callback])
 }
 
+const grep = ( elems, callback, invert ) => {
+    var callbackInverse;
+            var matches = [];
+            var i = 0;
+            var length = elems.length;
+            var callbackExpect = !invert
+
+    // Go through the array, only saving the items
+    // that pass the validator function
+    for ( ; i < length; i++ ) {
+        callbackInverse = !callback( elems[ i ], i )
+        if ( callbackInverse !== callbackExpect ) {
+            matches.push( elems[ i ] )
+        }
+    }
+
+    return matches
+}
+
 const unregisterMessage = function(type, sender) {
   if (type in callbacks) {
-    callbacks[type] = $.grep(callbacks[type], function(item) {
+    callbacks[type] = grep(callbacks[type], function(item) {
       return item[0] !== sender
     })
 
