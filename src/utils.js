480468ab38bd2e82b12a777a2746c07857735bac
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-12-24 15:35:23 +0300

    Add extend function to utils.js

--- src/utils.js
+++ src/utils.js
@@ -694,6 +694,84 @@ const callbacks = function(options) {
   return self
 }
 
+const extend = function() {
+  var options
+  var name
+  var src
+  var copy
+  var copyIsArray
+  var clone
+  var target = arguments[0] || {}
+  var i = 1
+  var length = arguments.length
+  var deep = false
+
+  // Handle a deep copy situation
+  if (typeof target === 'boolean') {
+    deep = target
+
+    // Skip the boolean and the target
+    target = arguments[i] || {}
+    i++
+  }
+
+  // Handle case when target is a string or something (possible in deep copy)
+  if (typeof target !== 'object' && !isFunction(target)) {
+    target = {}
+  }
+
+  // Extend jQuery itself if only one argument is passed
+  if (i === length) {
+    target = this
+    i--
+  }
+
+  for (; i < length; i++) {
+    // Only deal with non-null/undefined values
+    if ((options = arguments[i]) != null) {
+      // Extend the base object
+      for (name in options) {
+        copy = options[name]
+
+        // Prevent Object.prototype pollution
+        // Prevent never-ending loop
+        if (name === '__proto__' || target === copy) {
+          continue
+        }
+
+        // Recurse if we're merging plain objects or arrays
+        if (
+          deep &&
+          copy &&
+          (jQuery.isPlainObject(copy) || (copyIsArray = Array.isArray(copy)))
+        ) {
+          src = target[name]
+
+          // Ensure proper type for the source value
+          if (copyIsArray && !Array.isArray(src)) {
+            clone = []
+          } else if (!copyIsArray && !jQuery.isPlainObject(src)) {
+            clone = {}
+          } else {
+            clone = src
+          }
+          copyIsArray = false
+
+          // Never move original objects, clone them
+          target[name] = extend(deep, clone, copy)
+
+          // Don't bring in undefined values
+        } else if (copy !== undefined) {
+          target[name] = copy
+        }
+      }
+    }
+  }
+
+  // Return the modified object
+  return target
+}
+
 export {
   unique,
   defer,
@@ -727,5 +805,7 @@ export {
   taskRunner,
   fixedPipe,
   isFunction,
-  callbacks
+  callbacks,
+  inArray,
+  extend
 }
