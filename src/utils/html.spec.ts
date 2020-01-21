7f371e8aeeb8e56bd46f72627eeccb96b926780c
jeetiss <jeetiss@yandex.ru>
2020-01-21 09:17:46 +0300

    don't pass undefined and nulls in templates

--- src/utils/html.spec.ts
+++ src/utils/html.spec.ts
@@ -6,4 +6,10 @@ describe("HTML function", () => {
   it('should work with template string literals', () => {
     expect(html`<div></div>`).toBe('<div></div>')
   })
+
+  it('should skip falsy values from template (except 0)', () => {
+    expect(
+      html`<div null='${null}' undefined='${undefined}' number='${0}' true='${true}'>${false}</div>`
+    ).toBe("<div null='' undefined='' number='0' true='true'></div>")
+  })
 })
--- src/utils/html.ts
+++ src/utils/html.ts
@@ -3,9 +3,19 @@
 // For production 'babel-plugin-html-tag' removes html
 // function and evaluates and minifies tagged html
 
-type Base = string | number | null | undefined
+type Base = string | number | boolean |  null | undefined
+
+const validate = (value: Base, fallback: string = ''): string => {
+  if (value == null) {
+    return fallback
+  } else if (typeof value === 'boolean' && value === false) {
+    return fallback
+  } else {
+    return value.toString()
+  }
+}
 
 const html = (parts: TemplateStringsArray, ...values: Base[]): string =>
-  parts.reduce((str, next, i) => str + values[i - 1] + next)
+  parts.reduce((str, next, i) => str + validate(values[i - 1]) + next)
 
 export { html }
