0ff76f470379da2b0ccd18b9d573d87af6392714
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2020-02-04 15:46:02 +0800

    Comment for unresolved dependencies

--- src/widget/multiple-widget.js
+++ src/widget/multiple-widget.js
@@ -1,7 +1,5 @@
 import { BaseWidget } from './base-widget'
 import locale from '../locale'
-import { FileGroup } from '../files/group-creator'
-import { isFileGroupsEqual, valueToGroup } from '../utils/groups'
 import WidgetFile from '../file'
 
 class MultipleWidget extends BaseWidget {
@@ -10,21 +8,21 @@ class MultipleWidget extends BaseWidget {
     return (ref1 = this.currentObject) != null ? ref1.promise() : undefined
   }
 
-  __setObject(group) {
-    if (!isFileGroupsEqual(this.currentObject, group)) {
-      return super.__setObject(group)
-      // special case, when multiple widget is used with clearable
-      // and user or some external code clears the value after
-      // group loading error.
-    } else if (!group) {
-      this.__reset()
-      this.element.value = ''
-    }
-  }
+  // __setObject(group) {
+  //   if (!isFileGroupsEqual(this.currentObject, group)) {
+  //     return super.__setObject(group)
+  //     // special case, when multiple widget is used with clearable
+  //     // and user or some external code clears the value after
+  //     // group loading error.
+  //   } else if (!group) {
+  //     this.__reset()
+  //     this.element.value = ''
+  //   }
+  // }
 
   __setExternalValue(value) {
     var groupPr
-    this.__lastGroupPr = groupPr = valueToGroup(value, this.settings)
+    // this.__lastGroupPr = groupPr = valueToGroup(value, this.settings)
     if (value) {
       this.template.setStatus('started')
       this.template.statusText.textContent = locale.t('loadingInfo')
@@ -45,7 +43,7 @@ class MultipleWidget extends BaseWidget {
   __handleDirectSelection(type, data) {
     const files = data.map(file => new WidgetFile(file, this.settings))
     if (this.settings.systemDialog) {
-      return this.__setObject(FileGroup(files, this.settings))
+      // return this.__setObject(FileGroup(files, this.settings))
     } else {
       return this.__openDialog('preview').addFiles(files)
     }
