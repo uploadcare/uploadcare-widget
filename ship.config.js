aa56e214e200b5b287f052f028435e165186e2ff
jeetiss <jeetiss@yandex.ru>
2020-05-14 15:34:26 +0300

    setup shipjs

--- ship.config.js
+++ ship.config.js
@@ -1,1 +1,20 @@
-module.exports = { mergeStrategy: { toSameBranch: ['master'] } }
+module.exports = {
+  mergeStrategy: { toSameBranch: ['master'] },
+  pullRequestReviewers: ['jeetiss'],
+  buildCommand: () => null,
+  testCommandBeforeRelease: () => 'npm run test',
+  afterPublish: ({ exec }) => exec('node ./scripts/publish-to-s3.js'),
+  slack: {
+    // disable slack notification for `prepared` lifecycle.
+    // Ship.js will send slack message only for `releaseSuccess`.
+    prepared: null,
+  },
+  // skip preparation if master contain only `chore` commits
+  shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
+    const { fix = 0 } = commitNumbersPerType;
+    if (releaseType === "patch" && fix === 0) {
+      return false;
+    }
+    return true;
+  }
+}
