13f8e1225fe3d3915870ff18ebb8f6967b5fd7f3
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2020-06-17 20:55:03 +0300

    fix: apply exif to preview images only when browsers haven't done that yet (#764)
    * add nl
    * minimize test image
    * use better condition
    * setup onload callback before setting src

---
+++ src/utils/is-browser-apply-exif.js
@@ -0,0 +1,30 @@
+import $ from 'jquery'
+
+// 2x1 pixel image 90CW rotated with orientation header
+var testImageSrc = 'data:image/jpg;base64,' +
+  '/9j/4AAQSkZJRgABAQEASABIAAD/4QA6RXhpZgAATU0AKgAAAAgAAwESAAMAAAABAAYAAAEo' +
+  'AAMAAAABAAIAAAITAAMAAAABAAEAAAAAAAD/2wBDAP//////////////////////////////' +
+  '////////////////////////////////////////////////////////wAALCAABAAIBASIA' +
+  '/8QAJgABAAAAAAAAAAAAAAAAAAAAAxABAAAAAAAAAAAAAAAAAAAAAP/aAAgBAQAAPwBH/9k=';
+
+var isApplied
+var isBrowserApplyExif = () => {
+  var df = $.Deferred()
+
+  if (isApplied !== undefined) {
+    df.resolve(isApplied)
+  } else {
+    var image = new window.Image()
+  
+    image.onload = () => {
+      isApplied = image.naturalWidth < image.naturalHeight
+      image.src = '//:0'
+      df.resolve(isApplied)
+    }
+    image.src = testImageSrc
+  }
+
+  return df.promise()
+}
+
+export default isBrowserApplyExif
