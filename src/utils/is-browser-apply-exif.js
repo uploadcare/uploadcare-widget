927da3f87b39c4996b04f2ec4a2b177e0e2dcc76
jeetiss <jeetiss@yandex.ru>
2020-06-17 16:18:22 +0300

    minimize test image

--- src/utils/is-browser-apply-exif.js
+++ src/utils/is-browser-apply-exif.js
@@ -1,14 +1,11 @@
 import $ from 'jquery'
 
-var testImage = 'data:image/jpeg;base64,' +
-  '/9j/4QAiRXhpZgAATU0AKgAAAAgAAQESAAMAAAABAAYAAAAAAAD/7QA0UGhvdG9zaG9wIDMu' +
-  'MAA4QklNBAQAAAAAABccAgUAC2JsdWVpbXAubmV0HAIAAAIABAD/2wCEAAEBAQEBAQEBAQEB' +
-  'AQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEB' +
-  'AQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEB' +
-  'AQEBAQEBAQEBAf/AABEIAAIAAwMBEQACEQEDEQH/xABRAAEAAAAAAAAAAAAAAAAAAAAKEAEB' +
-  'AQADAQEAAAAAAAAAAAAGBQQDCAkCBwEBAAAAAAAAAAAAAAAAAAAAABEBAAAAAAAAAAAAAAAA' +
-  'AAAAAP/aAAwDAQACEQMRAD8AG8T9NfSMEVMhQvoP3fFiRZ+MTHDifa/95OFSZU5OzRzxkyej' +
-  'v8ciEfhSceSXGjS8eSdLnZc2HDm4M3BxcXwH/9k='
+// 2x1 pixel image 90CW rotated with orientation header
+var testImageSrc = 'data:image/jpg;base64,' +
+  '/9j/4AAQSkZJRgABAQEASABIAAD/4QA6RXhpZgAATU0AKgAAAAgAAwESAAMAAAABAAYAAAEo' +
+  'AAMAAAABAAIAAAITAAMAAAABAAEAAAAAAAD/2wBDAP//////////////////////////////' +
+  '////////////////////////////////////////////////////////wAALCAABAAIBASIA' +
+  '/8QAJgABAAAAAAAAAAAAAAAAAAAAAxABAAAAAAAAAAAAAAAAAAAAAP/aAAgBAQAAPwBH/9k=';
 
 var isApplied
 
@@ -20,7 +17,7 @@ var isBrowserApplyExif = () => {
   } else {
     var image = new window.Image()
     
-    image.src = testImage
+    image.src = testImageSrc
     image.onload = () => {
       isApplied = image.width === 2
       image.src = '//:0'
