3cfb0855e53a14e893121fc7b688f6956c5338ac
nd0ut <nd0ut.me@gmail.com>
2021-02-03 12:44:46 +0300

    fix: wrap canvas test into try/catch

--- src/utils/canvas-test.js
+++ src/utils/canvas-test.js
@@ -1,3 +1,5 @@
+import { log } from './warnings'
+
 const sizes = {
   squareSide: [
     // Synthetic limit
@@ -41,48 +43,54 @@ const FILL_STYLE = `rgba(${TestPixel.R}, ${TestPixel.G}, ${
 }, ${TestPixel.A / 255})`
 
 function canvasTest([width, height]) {
-  const fill = [width - 1, height - 1, 1, 1] // x, y, width, height
+  // Wrapped into try/catch because memory alloction errors can be thrown due to insufficient RAM
+  try {
+    const fill = [width - 1, height - 1, 1, 1] // x, y, width, height
 
-  const cropCvs = document.createElement('canvas')
-  cropCvs.width = 1
-  cropCvs.height = 1
-  const testCvs = document.createElement('canvas')
-  testCvs.width = width
-  testCvs.height = height
+    const cropCvs = document.createElement('canvas')
+    cropCvs.width = 1
+    cropCvs.height = 1
+    const testCvs = document.createElement('canvas')
+    testCvs.width = width
+    testCvs.height = height
 
-  const cropCtx = cropCvs.getContext('2d')
-  const testCtx = testCvs.getContext('2d')
+    const cropCtx = cropCvs.getContext('2d')
+    const testCtx = testCvs.getContext('2d')
 
-  if (testCtx) {
-    testCtx.fillStyle = FILL_STYLE
-    testCtx.fillRect.apply(testCtx, fill)
+    if (testCtx) {
+      testCtx.fillStyle = FILL_STYLE
+      testCtx.fillRect.apply(testCtx, fill)
 
-    // Render the test pixel in the bottom-right corner of the
-    // test canvas in the top-left of the 1x1 crop canvas. This
-    // dramatically reducing the time for getImageData to complete.
-    cropCtx.drawImage(testCvs, width - 1, height - 1, 1, 1, 0, 0, 1, 1)
+      // Render the test pixel in the bottom-right corner of the
+      // test canvas in the top-left of the 1x1 crop canvas. This
+      // dramatically reducing the time for getImageData to complete.
+      cropCtx.drawImage(testCvs, width - 1, height - 1, 1, 1, 0, 0, 1, 1)
+    }
+
+    const imageData = cropCtx && cropCtx.getImageData(0, 0, 1, 1).data
+    let isTestPass = false
+    if (imageData) {
+      // can't use array destructuring because transpiled code fails on IE10
+      // there, imageData have type CanvasPixelArray, not Uint8ClampedArray
+      const r = imageData[0]
+      const g = imageData[1]
+      const b = imageData[2]
+      const a = imageData[3]
+      // Verify test pixel (rgba channels should match TestPixel ones)
+      isTestPass =
+        r === TestPixel.R &&
+        g === TestPixel.G &&
+        b === TestPixel.B &&
+        a === TestPixel.A
+    }
+
+    testCvs.width = testCvs.height = 1
+
+    return isTestPass
+  } catch(e) {
+    log(`Failed to test for max canvas size of ${width}x${height}.`, e)
+    return false
   }
-
-  const imageData = cropCtx && cropCtx.getImageData(0, 0, 1, 1).data
-  let isTestPass = false
-  if (imageData) {
-    // can't use array destructuring because transpiled code fails on IE10
-    // there, imageData have type CanvasPixelArray, not Uint8ClampedArray
-    const r = imageData[0]
-    const g = imageData[1]
-    const b = imageData[2]
-    const a = imageData[3]
-    // Verify test pixel (rgba channels should match TestPixel ones)
-    isTestPass =
-      r === TestPixel.R &&
-      g === TestPixel.G &&
-      b === TestPixel.B &&
-      a === TestPixel.A
-  }
-
-  testCvs.width = testCvs.height = 1
-
-  return isTestPass
 }
 
 const test = () => {
