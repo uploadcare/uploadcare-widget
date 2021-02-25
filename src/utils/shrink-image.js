fe4d033aa829e895e609260904416222c0b15186
nd0ut <nd0ut.me@gmail.com>
2021-02-25 13:52:30 +0300

    chore: remake steps calculation loop on do-while instead of double condition while

--- src/utils/shrink-image.js
+++ src/utils/shrink-image.js
@@ -42,15 +42,17 @@ const calcShrinkSteps = function(sourceW, targetW, targetH, step) {
   const steps = []
   let sW = targetW
   let sH = targetH
-  steps.push([sW, sH])
-  while (sW < sourceW) {
+
+  // result should include at least one target step,
+  // even if abs(source - target) < step * source
+  // just to be sure nothing will break
+  // if the original resolution / target resolution condition changes
+  do {
+    steps.push([sW, sH])
     sW = Math.round(sW / step)
     sH = Math.round(sH / step)
+  } while (sW < sourceW * step)
 
-    if (sW < sourceW * step) {
-      steps.push([sW, sH])
-    }
-  }
   return steps.reverse()
 }
 
