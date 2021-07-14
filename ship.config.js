bda1275d58ad4f9ce3e91c3f1e031310b8b2f760
Aleksandr Grenishin <nd0ut.me@gmail.com>
2021-07-14 13:52:46 +0300

    chore: don't skip preparation for the patch changes (#850)

--- ship.config.js
+++ ship.config.js
@@ -8,13 +8,5 @@ module.exports = {
     // disable slack notification for `prepared` lifecycle.
     // Ship.js will send slack message only for `releaseSuccess`.
     prepared: null,
-  },
-  // skip preparation if master contain only `chore` commits
-  shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
-    const { fix = 0 } = commitNumbersPerType;
-    if (releaseType === "patch" && fix === 0) {
-      return false;
-    }
-    return true;
   }
 }
