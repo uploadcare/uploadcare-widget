747d8a27cd17c57438818641f365978f0207cbc0
Aleksandr Grenishin <nd0ut.me@gmail.com>
2023-06-06 15:45:36 +0300

    chore: revert build script run before test (#957)

--- ship.config.js
+++ ship.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   mergeStrategy: { toSameBranch: ['master'] },
   pullRequestReviewers: ['nd0ut'],
+  // build command is empty because it will be implicitly run by the test command below
+  buildCommand: () => null,
   testCommandBeforeRelease: () => 'npm run test',
   afterPublish: ({ exec }) => exec('node ./scripts/publish-to-s3.js')
 }
